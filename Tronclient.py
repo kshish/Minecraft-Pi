901916873677c79144d0ebd651597b1795c88d10
kshish <2049er@gmail.com>
2017-08-09 15:13:45 -0700

    Create Tronclient.py

---
+++ Tronclient.py
@@ -0,0 +1,32 @@
+from mcpi.minecraft import Minecraft
+import random
+from time import sleep
+running = True
+mc = Minecraft.create()
+time = 1
+while running:
+    x, y, z = mc.player.getPos()
+
+    iD = mc.getBlock(0,-60,0)
+
+    block = mc.getBlockWithData(x,y-1,z)
+
+
+    if iD == 1 and block.id == 35 and block.data == 14:
+        mc.setBlock(0, -60, 0, 0)
+
+    elif iD ==  0:
+        mc.player.setPos(0, -10000, 0)
+        mc = Minecraft.create("10.45.101.11")
+        mc.setBlock(0, -60, 0, 3)
+        mc = Minecraft.create()
+
+    elif iD ==  1:
+        mc.setBlock(x, y-1, z, 35, 11)
+
+    elif iD ==  2:
+        mc.setBlock(x, y-1, z, 35, 14)
+
+    elif iD == 3:
+        time = time-1
+        time = time + 1
