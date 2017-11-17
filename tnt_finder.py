276b4042f83c8d974973ed956015e747f7e8b160
kshish <2049er@gmail.com>
2017-11-17 16:36:44 +0000

    Update tnt_finder.py

--- tnt_finder.py
+++ tnt_finder.py
@@ -2,17 +2,6 @@ from mcpi.minecraft import Minecraft
 import random
 from time import sleep
 
-WOOL=35
-
-running = True
-finding = False
-
-from mcpi.minecraft import Minecraft
-import random
-from time import sleep
-
-WOOL=35
-
 running = True
 finding = False
 
@@ -27,19 +16,33 @@ WOOL=35
 
 running = True
 finding = False
+leap = 2
+
+global x
+global y
+global z
+global tnt_x
+global tnt_y
+global tnt_z
+global pl_x
+global pl_y
+global pl_z
 
 mc = Minecraft.create()
 #specify ip address and port
 #mc = minecraft.Minecraft.create("192.168.1.28", 4711)
 
-def start_tnt_find(
-    finding=True
+def start_tnt_find():
+
+
     tnt_x=x
     tnt_y=y
     tnt_z=z
+    
     entityIds = mc.getPlayerEntityIds()
     for entityId in entityIds:
-        print(i)
+        pl_x, pl_y, pl_z=mc.entity.getPos(entityId)
+
     #r = i % 2
 
     #if r > 0:
@@ -48,15 +51,32 @@ def start_tnt_find(
         #team.append("a")
 
     #print(team[i])
-    i = i + 1
+    
 
+def tnt_find():
+    if tnt_x > pl_x:
+        tnt_x=tnt_x-leap
+    if tnt_x > pl_x:
+        tnt_x=tnt_x+leap
+    if tnt_y > pl_y:
+        tnt_y=tnt_y-leap
+    if tnt_y > pl_y:
+        tnt_y=tnt_y+leap
+    if tnt_z > pl_z:
+        tnt_z=tnt_z-leap
+    if tnt_z > pl_z:
+        tnt_z=tnt_z+leap
+    mc.setBlock(tnt_x, tnt_y, tnt_z, 46,1)
+        
 
 while running:
     x, y, z = mc.player.getPos()
-    blockObj = mc.getBlockWithData(0,0,0)
-    if blockObj.id==WOOL:
-        if block.data==WHITE and finding==False:
-            start_tnt_find()
-            finding=True
-        elif finding=True:
-
+    blockObj = mc.getBlockWithData(x,y,z-1)
+    if blockObj.id==WOOL and finding==False:
+        start_tnt_find()
+        finding=True
+        
+    if finding==True:
+        tnt_find()
+    print (finding)
+    print(blockObj)
