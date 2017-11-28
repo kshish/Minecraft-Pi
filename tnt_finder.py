31312cd13fdc32a52cede63fcc5ff2421a23a993
kshish <2049er@gmail.com>
2017-11-28 16:41:11 +0000

    Update tnt_finder.py

--- tnt_finder.py
+++ tnt_finder.py
@@ -16,17 +16,18 @@ WOOL=35
 
 running = True
 finding = False
-leap = 2
+leap = 1
 
-global x
-global y
-global z
-global tnt_x
-global tnt_y
-global tnt_z
-global pl_x
-global pl_y
-global pl_z
+x=0
+y=0
+z=0
+tnt_x=0
+tnt_y=0
+tnt_z=0
+pl_x=0
+pl_y=0
+pl_z=0
+enemyId=0
 
 mc = Minecraft.create()
 #specify ip address and port
@@ -35,13 +36,18 @@ mc = Minecraft.create()
 def start_tnt_find():
 
 
+    global tnt_x
+    global tnt_y
+    global tnt_z
     tnt_x=x
     tnt_y=y
     tnt_z=z
+    global enemyId
     
     entityIds = mc.getPlayerEntityIds()
     for entityId in entityIds:
         pl_x, pl_y, pl_z=mc.entity.getPos(entityId)
+        enemyId=entityId
 
     #r = i % 2
 
@@ -54,24 +60,36 @@ def start_tnt_find():
     
 
 def tnt_find():
+    global pl_x
+    global pl_y
+    global pl_z
+    global tnt_x
+    global tnt_y
+    global tnt_z
+    
+    pl_x, pl_y, pl_z=mc.entity.getPos(enemyId)
     if tnt_x > pl_x:
         tnt_x=tnt_x-leap
-    if tnt_x > pl_x:
+    if tnt_x < pl_x:
         tnt_x=tnt_x+leap
     if tnt_y > pl_y:
         tnt_y=tnt_y-leap
-    if tnt_y > pl_y:
+    if tnt_y < pl_y:
         tnt_y=tnt_y+leap
     if tnt_z > pl_z:
         tnt_z=tnt_z-leap
-    if tnt_z > pl_z:
+    if tnt_z < pl_z:
         tnt_z=tnt_z+leap
     mc.setBlock(tnt_x, tnt_y, tnt_z, 46,1)
         
 
 while running:
+    global x
+    global y
+    global z
+    global finding
     x, y, z = mc.player.getPos()
-    blockObj = mc.getBlockWithData(x,y,z-1)
+    blockObj = mc.getBlockWithData(x,y-1,z)
     if blockObj.id==WOOL and finding==False:
         start_tnt_find()
         finding=True
